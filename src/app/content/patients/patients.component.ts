3a05c1f3a9561add65214bfdfbb9617dc6c424e9
dabare <dabaremadhava@gmail.com>
2019-05-09 22:22:02 +0530

    datatables nw working

--- src/app/content/patients/patients.component.ts
+++ src/app/content/patients/patients.component.ts
@@ -9,55 +9,34 @@ import { Observable } from 'rxjs';
   styleUrls: ['./patients.component.scss']
 })
 export class PatientsComponent implements OnInit {
-  private patients: any;
+  public allPatients: any[];
 
   constructor(private databaseService: DatabaseService) { }
 
-
   ngOnInit() {
     this.initTable();
-    var myVar = setInterval(this.checkTable, 1000);
   }
 
-  dataSet = false;
-
-  checkTable(){
-    alert(!this.dataSet && (this.patients.length == $('#editable tbody tr').length));
-    if(!this.dataSet && (this.patients.length == $('#editable tbody tr').length)){
-      this.dataSet = true;
-      this.initTable();
-      alert($('#editable tbody tr').length);
-    }
-  }
 
   getPatients() {
-    this.datatable.destroy();
- 
-    this.databaseService.getPatients().subscribe((data: []) => {
-        this.patients = data;
-        this.dataSet = false;
+    //this.datatable.destroy();
 
-        setTimeout(() => {
-          if($('#editable tbody tr').length == this.patients.length ){
-            this.initTable();
-          }
-        }, 1000);
-        
-      }, (err) => {
-        console.log(err);
-      }
- 
+    this.databaseService.getPatients().subscribe((data: any) => {
+      this.allPatients = data;
+
+      this.datatable.clear();
+      this.datatable.rows.add(data);
+      this.datatable.draw();
+      this.resetTableListners();
+    }, (err) => {
+      console.log(err);
+    }
     );
   }
 
 
   savePatient() {
-    this.databaseService.query("INSERT INTO patient VALUES(4,'testing','134123')").subscribe((data: []) => {
-      console.log(data);
-    }, (err) => {
-      console.log(err);
-    }
-    );
+
   }
 
 
@@ -79,10 +58,31 @@ export class PatientsComponent implements OnInit {
     });
   }
 
-  datatable:any;
+  datatable: any;
 
   initTable() {
     this.datatable = (<any>$('#editable')).DataTable({
+      "pagingType": "full_numbers",
+      responsive: true,
+      columns: [
+        {
+          data: "idpatient"
+        },
+        {
+          data: "idpatient"
+        },
+        {
+          data: "name"
+        },
+        {
+          data: "contactNo"
+        },
+        //create three buttons columns
+        {
+          defaultContent: `<button type="button" class="btn btn-xs btn-warning showIdButton"><span class="glyphicon glyphicon-edit"></span>
+          </button>`
+        }
+      ],
       "columnDefs": [
         {
           "searchable": false,
@@ -100,6 +100,37 @@ export class PatientsComponent implements OnInit {
     });
   }
 
-   
-  
+  resetTableListners() {
+    //store current class reference in _currClassRef variable for using in jquery click event handler
+    var _currClassRef = this;
+
+    //unbind previous event on tbody so that multiple events are not binded to the table whenever this function runs again
+    $('#editable tbody td').unbind();
+
+    //defined jquery click event
+    $('#editable tbody td').on('click', 'button', function () {
+      //the "this" in this function is "this" of jquery object not of component because we did not use an arrow function
+
+      //get row for data
+      var tr = $(this).closest('tr');
+      var row = _currClassRef.datatable.row(tr);
+      //this of jquery object
+      if ($(this).hasClass("showFButton")) {
+        //use function of current class using reference
+        _currClassRef.showValue(row.data().FirstName);
+      }
+      else if ($(this).hasClass("showLButton")) {
+        _currClassRef.showValue(row.data().LastName);
+      }
+      else if ($(this).hasClass("showIdButton")) {
+        _currClassRef.showValue(row.data().idpatient);
+      }
+
+    })
+  }
+
+  showValue(value) {
+    alert(value)
+  }
+
 }
