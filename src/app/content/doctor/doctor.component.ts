d37640284b36b886cb0e9cb803e9c33801b8ff22
dabare <dabaremadhava@gmail.com>
2019-05-12 11:18:00 +0530

    Doctor calendar

--- src/app/content/doctor/doctor.component.ts
+++ src/app/content/doctor/doctor.component.ts
@@ -8,8 +8,9 @@ import { DoctorService } from './doctor.service';
 })
 export class DoctorComponent implements OnInit {
   private datatable: any;
+  private fullCalendar:any;
   private doctors: any[];
-
+  private events: any[];
   private mode = "";
 
   private doctor = {
@@ -118,177 +119,78 @@ export class DoctorComponent implements OnInit {
   
 
   initCalendar() {
-    /* initialize the external events
-     -----------------------------------------------------------------*/
-
-
-    (<any>$('#external-events div.external-event')).each(function () {
-
-      // store data so the calendar knows to render an event upon drop
-      $(this).data('event', {
-        title: $.trim($(this).text()), // use the element's text as the event title
-        stick: true // maintain when user navigates (see docs on the renderEvent method)
-      });
-
-      // make the event draggable using jQuery UI
-      (<any>$(this)).draggable({
-        zIndex: 1111999,
-        revert: true,      // will cause the event to go back to its
-        revertDuration: 0  //  original position after the drag
-      });
-
-    });
-
-    /* initialize the calendar
-     -----------------------------------------------------------------*/
-    var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
-
-    (<any>$('#calendar')).fullCalendar({
+    this.fullCalendar = (<any>$('#calendar')).fullCalendar({
+      defaultView: 'month',
+      height: "auto",
       header: {
         left: 'prev,next today',
         center: 'title',
         right: 'month,agendaWeek,agendaDay'
       },
-      editable: true,
-      droppable: true, // this allows things to be dropped onto the calendar
-      drop: function () {
-        console.log($(this));
-        // is the "remove after drop" checkbox checked?
-        if ($('#drop-remove').is(':checked')) {
-          // if so, remove the element from the "Draggable Events" list
-          $(this).remove();
-        }
-      },
-      events: [
-        {
-          title: 'All Day Event',
-          start: new Date(y, m, 1)
-        },
-        {
-          title: 'Long Event',
-          start: new Date(y, m, d - 5),
-          end: new Date(y, m, d - 2)
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: new Date(y, m, d - 3, 16, 0),
-          allDay: false
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: new Date(y, m, d + 4, 16, 0),
-          allDay: false
-        },
-        {
-          title: 'Meeting',
-          start: new Date(y, m, d, 10, 30),
-          allDay: false
-        },
-        {
-          title: 'Lunch',
-          start: new Date(y, m, d, 12, 0),
-          end: new Date(y, m, d, 14, 0),
-          allDay: false
-        },
-        {
-          title: 'Birthday Party',
-          start: new Date(y, m, d + 1, 19, 0),
-          end: new Date(y, m, d + 1, 22, 30),
-          allDay: false
-        },
-        {
-          title: 'Click for Google',
-          start: new Date(y, m, 28),
-          end: new Date(y, m, 29),
-          url: 'http://google.com/'
-        }
-      ],
+      editable: false,
+      events: this.events,
       eventClick: function (info) {
         console.log(info);
-      },
-      dateClick: function (info) {
-        console.log(info);
-      },
-      navLinks: true,
-      navLinkDayClick: function (date, jsEvent) {
-        console.log('day', date.toISOString());
-        console.log('coords', jsEvent.pageX, jsEvent.pageY);
       }
     });
   }
 
 
-  // initCalendar(){
-  //   var calendarEl = document.getElementById('calendar');
+  addEvents(){
 
-  //   var calendar = new Calendar(calendarEl, {
-  //     schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
-  //     plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list', 'resourceTimeline' ],
-  //     now: '2019-04-07',
-  //     editable: true, // enable draggable events
-  //     aspectRatio: 1.8,
-  //     scrollTime: '00:00', // undo default 6am scrollTime
-  //     header: {
-  //       left: 'today prev,next',
-  //       center: 'title',
-  //       right: 'resourceTimelineDay,resourceTimelineThreeDays,timeGridWeek,dayGridMonth,listWeek'
-  //     },
-  //     defaultView: 'resourceTimelineDay',
-  //     views: {
-  //       resourceTimelineThreeDays: {
-  //         type: 'resourceTimeline',
-  //         duration: { days: 3 },
-  //         buttonText: '3 days'
-  //       }
-  //     },
-  //     resourceLabelText: 'Rooms',
-  //     resources: [
-  //       { id: 'a', title: 'Auditorium A' },
-  //       { id: 'b', title: 'Auditorium B', eventColor: 'green' },
-  //       { id: 'c', title: 'Auditorium C', eventColor: 'orange' },
-  //       { id: 'd', title: 'Auditorium D', children: [
-  //         { id: 'd1', title: 'Room D1' },
-  //         { id: 'd2', title: 'Room D2' }
-  //       ] },
-  //       { id: 'e', title: 'Auditorium E' },
-  //       { id: 'f', title: 'Auditorium F', eventColor: 'red' },
-  //       { id: 'g', title: 'Auditorium G' },
-  //       { id: 'h', title: 'Auditorium H' },
-  //       { id: 'i', title: 'Auditorium I' },
-  //       { id: 'j', title: 'Auditorium J' },
-  //       { id: 'k', title: 'Auditorium K' },
-  //       { id: 'l', title: 'Auditorium L' },
-  //       { id: 'm', title: 'Auditorium M' },
-  //       { id: 'n', title: 'Auditorium N' },
-  //       { id: 'o', title: 'Auditorium O' },
-  //       { id: 'p', title: 'Auditorium P' },
-  //       { id: 'q', title: 'Auditorium Q' },
-  //       { id: 'r', title: 'Auditorium R' },
-  //       { id: 's', title: 'Auditorium S' },
-  //       { id: 't', title: 'Auditorium T' },
-  //       { id: 'u', title: 'Auditorium U' },
-  //       { id: 'v', title: 'Auditorium V' },
-  //       { id: 'w', title: 'Auditorium W' },
-  //       { id: 'x', title: 'Auditorium X' },
-  //       { id: 'y', title: 'Auditorium Y' },
-  //       { id: 'z', title: 'Auditorium Z' }
-  //     ],
-  //     events: [
-  //       { id: '1', resourceId: 'b', start: '2019-04-07T02:00:00', end: '2019-04-07T07:00:00', title: 'event 1' },
-  //       { id: '2', resourceId: 'c', start: '2019-04-07T05:00:00', end: '2019-04-07T22:00:00', title: 'event 2' },
-  //       { id: '3', resourceId: 'd', start: '2019-04-06', end: '2019-04-08', title: 'event 3' },
-  //       { id: '4', resourceId: 'e', start: '2019-04-07T03:00:00', end: '2019-04-07T08:00:00', title: 'event 4' },
-  //       { id: '5', resourceId: 'f', start: '2019-04-07T00:30:00', end: '2019-04-07T02:30:00', title: 'event 5' }
-  //     ]
-  //   });
-
-  //   calendar.render();
-  // }
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+    this.events = [
+      {
+        title: 'All Day Event',
+        start: new Date(y, m, 1)
+      },
+      {
+        title: 'Long Event',
+        start: new Date(y, m, d - 5),
+        end: new Date(y, m, d - 2)
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: new Date(y, m, d - 3, 16, 0),
+        allDay: false
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: new Date(y, m, d + 4, 16, 0),
+        allDay: false
+      },
+      {
+        title: 'Meeting',
+        start: new Date(y, m, d, 10, 30),
+        allDay: false
+      },
+      {
+        title: 'Lunch',
+        start: new Date(y, m, d, 12, 0),
+        end: new Date(y, m, d, 14, 0),
+        allDay: false
+      },
+      {
+        title: 'Birthday Party',
+        start: new Date(y, m, d + 1, 19, 0),
+        end: new Date(y, m, d + 1, 22, 30),
+        allDay: false
+      },
+      {
+        title: 'Click for Google',
+        start: new Date(y, m, 28),
+        end: new Date(y, m, 29),
+        url: 'http://google.com/'
+      }
+    ];
+    this.fullCalendar.fullCalendar('getCalendar').removeEvents();
+    this.fullCalendar.fullCalendar('getCalendar').addEventSource(this.events);
+  }
 
   initSelect() {
     (<any>$(".select2_demo_3")).select2({
